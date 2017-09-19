f09f3d88da1d48654607570079396f5a6ff1de34
Shagor Hasan <shagoraust@gmail.com>
2017-09-19 21:46:12 +0600

    Update MainActivity.java

--- app/src/main/java/com/example/shago_000/filetransfer/MainActivity.java
+++ app/src/main/java/com/example/shago_000/filetransfer/MainActivity.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
